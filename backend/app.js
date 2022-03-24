759e6e605fd248d16abde9c97d4d143e2beb1eab
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-03-24 13:41:38 +0100

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,8 @@
 
-// MongoDB Logs
-// User1
-// 5PF7SUzSlYK3cevy
-
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const Thing = require('./models/thing');
 
 mongoose.connect('mongodb+srv://User1:5PF7SUzSlYK3cevy@cours-gofullstack.4stfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
    useNewUrlParser: true,
@@ -24,35 +21,24 @@ app.use((req, res, next) => {
     // Envoie des requêtes avec les méthodes mentionnées
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-})
+});
 
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-          },
-          {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-          },
-    ];
-    res.status(200).json(stuff);
-})
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({error}));
+});
 
 app.post('api/stuff', (req, res, next) => {
-    console.log(res.body);
-    res.status(201).json({
-        message: 'Objet créé !'
+    delete req.body._id;
+
+    const thing = new Thing({
+        ...req.body
     });
+
+    thing.save()
+        .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({error}));
 });
 
 module.exports = app;
