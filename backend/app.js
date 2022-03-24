64f6549ae4f57a3caae349e04e355e9388b9eef2
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-03-24 13:22:39 +0100

    Créez un schéma de données

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,58 @@
-const express = require('express');
 
+// MongoDB Logs
+// User1
+// 5PF7SUzSlYK3cevy
+
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Requete recue !');
-    next();
-});
+mongoose.connect('mongodb+srv://User1:5PF7SUzSlYK3cevy@cours-gofullstack.4stfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true 
+})
+.then(() => console.log('Connexion a MongoDB réussi !'))
+.catch(() => console.log('Connexion a MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.status(201);
+    // Acces à l'API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Ajout des headers mentionnés aux requêtes envoyées vers l'API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // Envoie des requêtes avec les méthodes mentionnées
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+})
 
-app.use((req, res, next) => {
-    res.json({message: 'Votre requete a bien ete recue !'});
-    next();
-});
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+          },
+          {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+          },
+    ];
+    res.status(200).json(stuff);
+})
 
-app.use((req, res, next) => {
-    console.log('Reponse envoyée avec succès !');
+app.post('api/stuff', (req, res, next) => {
+    console.log(res.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
 });
 
 module.exports = app;
